53e6ea089fcf485075812900889474ddcb3f9a54
LebrunAlexandre <alexandre.lebrun@inhaus.de>
2025-10-31 13:48:01 +0100

    #5318 changes for linux-arm

--- hyper/publish.cmd
+++ hyper/publish.cmd
@@ -3,7 +3,7 @@ REM danach ist der x bit nicht gesetzt. In linux mit "chmod +x hyper ClientTCP" 
 SET sevenZ="C:\Program Files\7-Zip\7z.exe"
 PUSHD ..
 if exist publishlinux-arm.zip del publishlinux-arm.zip
-dotnet publish -c Release --self-contained -r linux-arm -o ./publishlinux-arm /p:PublishTrimmed=true
+dotnet publish -c Release --self-contained -r linux-arm -o ./publishlinux-arm
 if exist %sevenZ% %sevenZ% a -tzip publishlinux-arm.zip publishlinux-arm\*
 explorer .
 POPD
