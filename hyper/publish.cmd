9e9f2f5e90d5a8f497076deca4034c5a5073bd8f
LebrunAlexandre <alexandre.lebrun@inhaus.de>
2025-11-03 09:17:36 +0100

    #5138 linq2db+sqllite v3.5.1 newer version of sqllite don't work. still problems with System.IO.Ports on linux-arm

--- hyper/publish.cmd
+++ hyper/publish.cmd
@@ -1,11 +1,17 @@
 REM publish for raspi
 REM danach ist der x bit nicht gesetzt. In linux mit "chmod +x hyper ClientTCP" setzen
 SET sevenZ="C:\Program Files\7-Zip\7z.exe"
+SET RuntimeId=linux-arm
+SET Framework=net8
 PUSHD ..
-if exist publishlinux-arm.zip del publishlinux-arm.zip
-dotnet publish -c Release --self-contained -r linux-arm -o ./publishlinux-arm
-if exist %sevenZ% %sevenZ% a -tzip publishlinux-arm.zip publishlinux-arm\*
+if exist publish%RuntimeId%.zip del publish%RuntimeId%.zip
+dotnet publish -c Release --self-contained -r %RuntimeId%
+robocopy ClientTCP\bin\Release\%Framework%\%RuntimeId%\publish publish%RuntimeId%\
+robocopy hyper\bin\Release\%Framework%\%RuntimeId%\publish publish%RuntimeId%\
+
+if exist %sevenZ% %sevenZ% a -tzip publish%RuntimeId%.zip publish%RuntimeId%\*
 explorer .
 POPD
 ECHO This window will close automatically in 60 seconds...
-ping -n 61 localhost > /nul
+REM ping -n 61 localhost > /nul
+timeout /t 60
